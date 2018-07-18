b815ddaf3eb2fbd86581c4ce54d5effb675a15d7
Pradeep Mishra <prdpkmishra@gmail.com>
2018-07-18 10:08:49 +0530

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+	$(".pop").on("click", function() {
+	   $('#imagepreview').attr('src', $(this).children('.imageresource').attr('src')); // here asign the image to the modal when the user click the enlarge link
+	   $('#imagemodal').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
+	});
+});
+
+//Scroll to top
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
